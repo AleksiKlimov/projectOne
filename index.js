faf6947ec3dff52dc5c5dea7c7206209caebbf0f
AleksiKlimov <kuzmenkoaleksej59@gmail.com>
2022-08-17 12:08:06 +0300

    newconnit3

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function name(params) {
   console.log("hi my name is cool boy");
+  console.log:('works with Git')
 }
 console.log("hi baby, i love you very much");
-console.log("i love beatiful girl");
+console.log("i love beatifuul girl");
 console.log("i love beatiful girl");
 console.log("i love beatiful girl");
 console.log("i love beatiful girl");

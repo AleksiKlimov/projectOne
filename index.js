53d18bac6c247bb4ecf368aa474ecb2acdf3a4f6
AleksiKlimov <kuzmenkoaleksej59@gmail.com>
2022-08-25 09:53:34 +0300

    new commit 3 25

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function name(params) {
-   console.log('hi my name is cool boy')
+  console.log("hi my name is cool boy");
+  if (5 > 2) {
+    console.log("it's true");
+  }
 }
-console.log('hi baby, i love you very much')
-console.log('i love beatiful girl')
-console.log('i love beatiful girl')
-
+console.log("hi baby, i love you very much");
+console.log("i love beatiful girl");
+console.log("i love beatiful girl");
